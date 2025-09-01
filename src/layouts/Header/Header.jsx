79fdd29a7a78c7a8bd5a691701b449bc52a21d78
heroldserg89 <62557715+heroldserg89@users.noreply.github.com>
2025-09-01 06:05:41 +0300

    добавляет меню в хедере (#3)

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,7 +1,10 @@
 import './Header.scss'
 import Logo from '@/components/Logo'
+import getPath from '@/utils/getPath'
+import clsx from 'clsx'
 
-export default () => {
+export default (props) => {
+  const { url } = props
   const menuList = [
     {
       label: 'Home',
@@ -23,13 +26,18 @@ export default () => {
 
   return (
     <header className="header">
-      <div className="header__wraper container">
+      <div className="header__wrapper container">
         <Logo className="header__logo" loading="eager" />
         <nav className="header__nav">
           <ul className="header__menu">
             {menuList.map(({ label, href }, index) => (
               <li className="header__menu-item" key={index}>
-                <a className="header__menu-link" href={href}>
+                <a
+                  className={clsx('header__menu-link', {
+                    'header__menu-link--active': href === url,
+                  })}
+                  href={getPath(href)}
+                >
                   {label}
                 </a>
               </li>
