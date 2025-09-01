137514e6e654a0022e880673b019b07083b8422d
herold89 <heroldserg89@gmail.com>
2025-09-01 05:12:20 +0300

    тестирует ссылки

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -2,10 +2,40 @@ import './Header.scss'
 import Logo from '@/components/Logo'
 
 export default () => {
+  const menuList = [
+    {
+      label: 'Home',
+      href: '/',
+    },
+    {
+      label: 'Movies & Shows',
+      href: '/movies',
+    },
+    {
+      label: 'Support',
+      href: '/support',
+    },
+    {
+      label: 'Subscriptions',
+      href: '/subscriptions',
+    },
+  ]
+
   return (
     <header className="header">
       <div className="header__wraper container">
         <Logo className="header__logo" loading="eager" />
+        <nav className="header__nav">
+          <ul className="header__menu">
+            {menuList.map(({ label, href }, index) => (
+              <li className="header__menu-item" key={index}>
+                <a className="header__menu-link" href={href}>
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </header>
   )
