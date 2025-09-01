d68c2dd4ef4333c743e0c18bc6b8361c14efc6c6
herold89 <heroldserg89@gmail.com>
2025-09-01 04:57:48 +0300

    исправляет eslintconfig

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,17 @@
 import config from 'friendly-frontend-lint-config/eslint'
 
-export default config
+export default [
+  ...config,
+  {
+    rules: {
+      'jsx-a11y/anchor-is-valid': [
+        'error',
+        {
+          components: ['Link'],
+          specialLink: ['hrefLeft', 'hrefRight'],
+          aspects: ['preferButton'],
+        },
+      ],
+    },
+  },
+]
